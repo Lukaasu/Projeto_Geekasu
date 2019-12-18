4e620fb8b961cdc41a19301896d5ed720ea9290c
Lukaasu <lukasuoffcial@gmail.com>
2019-12-17 21:37:14 -0300

    Projeto UC 12

---
+++ geekasu/produtos-editar-post.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'classes/Produtos.php';
+
+$produto = new Produtos();
+
+$id = $_POST['id'];
+$produto_formulario = $_POST['produto'];
+$preco = $_POST['preco'];
+$quantidade = $_POST['quantidade'];
+$categoria = $_POST['categoria'];
+$editora = $_POST['editora'];
+$volume = $_POST['volume'];
+$distribuidora = $_POST['distribuidora'];
+
+
+$produto->alterar($id, $produto_formulario, $preco, $quantidade, $categoria, $editora, $volume, $distribuidora);
+
+header('Location:produtos.php')
+
+
+
+
+?>
